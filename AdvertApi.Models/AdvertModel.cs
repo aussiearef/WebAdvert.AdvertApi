8080a6b1dc9b3662232f3f087d9cf9e2af4732a2
aussiearef <arefkr@gmail.com>
2019-01-04 18:59:19 -0800

    Added GetAll method to the api

--- AdvertApi.Models/AdvertModel.cs
+++ AdvertApi.Models/AdvertModel.cs
@@ -6,5 +6,6 @@
         public string Description { get; set; }
         public double Price { get; set; }
         public string UserName { get; set; }
+        public string FilePath { get; set; }
     }
 }
